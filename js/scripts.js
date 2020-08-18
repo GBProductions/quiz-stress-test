b314097c8b29a75385fa2226eab5349164bae4da
Sean Wilson <paintballsean@gmail.com>
2020-08-18 14:34:00 -0700

    Commit changes soome of the form responses.
    Co-authored-by: Garrett Brown <garrettpaulbrown@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,11 @@
 $(document).ready(function() {
-  $("").submit(function(event) {
-  event.preventDefault();
-
+  $("form#stressTestQuiz").submit(function(event) {
+    event.preventDefault();
+    $("#stress-responses").show();
+    $("input:checkbox[name=work-transportation]:checked").each(function() {
+      const stressResponseAnswers = $(this).val();
+      $('#work-responses').append(stressResponseAnswers + "<br>");
+    });
+    $('#transportation_survey').hide();
   });
 });
